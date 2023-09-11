c2ddad30e5f7a6255e1b6420e61716cb2ac24ef1
bhismapratama <bhismaelki@gmail.com>
2023-09-11 16:16:30 +0700

    docs: initialisasi vue js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <div class="wrap">
+      <h1>haloo</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+*, html, body {
+  font-family: 'poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
